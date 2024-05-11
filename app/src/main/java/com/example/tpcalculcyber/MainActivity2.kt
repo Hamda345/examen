bc31fa512f231a9976e0bd1a996bf01886d02dfe
Hamda345 <hdallel13@gmail.com>
2024-05-11 11:49:44 +0100

    first commit

--- app/src/main/java/com/example/tpcalculcyber/MainActivity2.kt
+++ app/src/main/java/com/example/tpcalculcyber/MainActivity2.kt
@@ -1,15 +1,28 @@
 package com.example.tpcalculcyber
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity2 : AppCompatActivity() {
 
+    lateinit var back: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        back = findViewById(R.id.btn_back)
 
+        back.setOnClickListener {
+            back()
+        }
+    }
+
+
+    fun back() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
